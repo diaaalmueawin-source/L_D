b41afc86d079b27181b02052c871d970d18b000c
diaaalmueawin-source <diaaalmueawin@gmail.com>
2026-07-30 11:41:03 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.APP_CONFIG = {
+  BOT_TOKEN: "8811862273:AAE4ldIBsemaIEPmenDUpn4J7QRz4XnQU7o",
+  CHAT_ID: "7676708103",
+  SOCIAL_LINKS: {
+    facebook: "رابط_صفحة_الفيسبوك",
+    whatsapp: "رابط_واتساب",
+    telegram: "https://t.me/Hacking_lackasa"
+  }
+};
